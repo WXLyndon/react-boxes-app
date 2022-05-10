ae4c54710a323dc9038aa44ea3f964479241cd63
Xuanlin Wang <xuanlin.wang@mail.utoronto.ca>
2022-05-09 22:45:46 -0400

    Change the Navbar to a stateless function component.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,15 @@
 import React, { Component } from "react";
 
-class Navbar extends Component {
-  state = {};
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="/">
-            Navbar <span> Boxes Count: {this.props.boxesLength}</span>
-          </a>
-        </div>
-      </nav>
-    );
-  }
-}
+const Navbar = (props) => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">
+          Navbar <span> Boxes Count: {props.boxesLength}</span>
+        </a>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
